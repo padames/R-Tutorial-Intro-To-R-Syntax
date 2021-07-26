90abc6c43662b2880a85c41164ccbc5f88663199
Pablo Adames <pablo.adames@gmail.com>
2021-07-25 20:11:55 -0600

    coin tossing simulation and gif

--- Probabilities/coin_tossing_simulation3.R
+++ Probabilities/coin_tossing_simulation3.R
@@ -1,11 +1,13 @@
-#!/usr/bin/r
+#!/usr/bin/Rscript
 
-i_am_on_path <- dirname(rstudioapi::getSourceEditorContext()$path)
+library(here)
 
-if (!file.exists(file.path(i_am_on_path,"animations"))) {
-  dir.create(file.path(i_am_on_path, "animations"))
+suppressWarnings(my_proj_path <- here("Probabilities"))
+
+if (!file.exists(file.path(my_proj_path,"animations"))) {
+  dir.create(file.path(my_proj_path, "animations"))
 }
-setwd(file.path(i_am_on_path, "animations"))
+setwd(file.path(my_proj_path, "animations"))
 
 library(lattice)
 
@@ -15,20 +17,26 @@ png(file="simsample%02d.png", width=600, height=400)
 for (i in 1:30){
   coin_toss_vector = sample(c("Head", "Tail"), num_samples, replace=T)
   z = cumsum( coin_toss_vector == "Head") / seq(1, num_samples)
+  # data.ct <- data.frame(CoinTossNumber=1:length(z), AvgHeads=z)
   x = seq(1,num_samples)
+  
   pp <- xyplot( z ~ x,
-                scales=list(cex=.8, col="red", x = list(log = T), y = list(log = F)),
+                cex = 1.2,
+                scales=list(cex=1.2, col="black", relation = "same",
+                            x = list(log = 10, at=c(1,10,100,1000,10000), labels=c("1","10","100","1000","10000"))),
+                # scales=list(cex=.8, col="red", x = list(log = T), y = list(log = F)),
                 main="Trend of average number of Heads as number of coin tosses increases",
                 ylab = "Average Number of Heads",
-                xlim = c(1, num_samples),
+                xlim = c(0.9, num_samples+1),
                 xlab = "Number of coin tosses",
-                ylim = c(0, 1),
-                panel = function(x, y) {
+                ylim = c(-0.1, 1.1),
+                panel = function(x,y) {
                   panel.xyplot(x,y, type = 'l');
-                  panel.lines(x=log10(1:num_samples), y=rep(0.5,num_samples), col="red")
+                  panel.lines(x=log(1:num_samples), y=rep(0.5,num_samples), col="red")
                 }
   )
   print(pp)
+  
 }
 dev.off()
 
